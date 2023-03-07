4c6f648435d21682384cda9f01e0102cb941fa06
Trustlin <esosuakpotrustee@gmail.com>
2023-03-07 05:46:38 -0800

    python if/else

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# print 0 to 98 in both hexadecimal and decimal
+for Hexadec in range(0, 99):
+    print("{} = {}".format(Hexadec, hex(Hexadec)))
---
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -0,0 +1,20 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to next node
+ */
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+listint_t *insert_node(listint_t **head, int number);
+
+#endif
