fe80598d4b99e62bf086705bc79be1c1ff333bff
Trustlin <esosuakpotrustee@gmail.com>
2023-03-07 12:14:19 -0800

    Python if/else, loops

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-# 2-print_alphabet.py
-
-"""print the alphabet in lowercase, not followed by a new line, """
-for letter in range(97, 123):
-    print("{}".format(char(letter)), end="")
+for i in range(97, 123):
+    tableAsciiToAlphabet = chr(i)
+    asciiToAlpha = "" + tableAsciiToAlphabet
+    print("{}".format(asciiToAlpha), end='')
